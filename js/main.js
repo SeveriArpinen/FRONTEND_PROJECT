5d2c85be3bb9dc6c206c7085d6b7d5701ffb2b04
SeveriArpinen <severi.arpinen03@gmail.com>
2024-11-07 14:23:20 +0200

    Added responsiveness for mobile- and smaller screens!

--- js/main.js
+++ js/main.js
@@ -34,3 +34,13 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 });
+
+// MObile menu
+document.addEventListener("DOMContentLoaded", () => {
+  const hamburgerButton = document.querySelector(".hamburger-button");
+  const mobileMenu = document.querySelector(".mobile-menu");
+
+  hamburgerButton.addEventListener("click", () =>
+    mobileMenu.classList.toggle("active")
+  );
+});
